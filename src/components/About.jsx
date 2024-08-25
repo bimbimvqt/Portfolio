2e25f14235327bfe24ab2f76086efe2f594624da
TuanVuong <61371632+bimbimvqt@users.noreply.github.com>
2024-08-25 21:38:37 +0700

    init

---
+++ src/components/About.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]"
+      >
+        I have a strong foundation in programming and problem-solving skills.
+        I'm always eager to learn new technologies and improve my skills. I'm a
+        team player and I can work well in a team environment. I'm also a
+        self-motivated person and I can work well independently. I'm a fast
+        learner and I can adapt to new environments quickly. I'm a creative
+        person and I can come up with innovative solutions to problems. I'm a
+        responsible person and I always deliver my work on time. I'm a positive
+        person and I always have a positive attitude towards work.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, "about");
